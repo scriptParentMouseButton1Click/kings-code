df864b3b1e5c3d14194076c81a9bc56d6893f987
scriptParentMouseButton1Click <74497801+scriptParentMouseButton1Click@users.noreply.github.com>
2020-12-29 21:59:16 +0000

    Create pong.py

---
+++ pong.py
@@ -0,0 +1,135 @@
+#Simple Pong game
+
+import winsound
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#Score
+
+score_a = 0
+score_b = 0
+
+#Paddle a
+
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+#Paddle b
+
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+#Ball
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.2
+ball.dy = 0.2
+
+#Pen
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+#Functions
+
+def paddleaup():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddleadown():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddlebup():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddlebdown():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+#Keybinds
+
+wn.listen()
+wn.onkeypress(paddleaup, "w")
+wn.onkeypress(paddleadown, "s")
+wn.onkeypress(paddlebup, "Up")
+wn.onkeypress(paddlebdown, "Down")
+
+while True:
+    wn.update()
+
+    #Ball move
+
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border Checking
+
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+    
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+    # Paddle and ball collisions
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
